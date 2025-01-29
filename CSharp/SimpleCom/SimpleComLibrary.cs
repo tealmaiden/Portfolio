8ac6f93547de2d859b3067015484639f64892ec6
Nova <89001003490513@fedidcard.gov>
2025-01-28 16:41:02 -0800

    new projects that attempt to connect c# in .net 9 to excel vba using registered and registration-free com, respectively

---
+++ CSharp/SimpleCom/SimpleComLibrary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// 🔹 Define the Type Library GUID (Matches the Library GUID in IDL)
+[assembly: Guid("12345678-ABCD-1234-ABCD-1234567890AB")]
+
+namespace SimpleComLibrary
+{
+	[ComVisible(true)] // Makes the interface visible to COM
+	[Guid("87654321-DCBA-4321-DCBA-0987654321BA")] // Interface GUID (Matches IDL Interface GUID)
+	public interface ISimpleCom
+	{
+		string Test(); // A simple method that returns a "Hello" string
+	}
+
+	[ComVisible(true)] // Makes the class visible to COM
+	[Guid("98765432-CBA1-4321-CBA1-1234567890AB")] // Class GUID (Matches IDL CoClass GUID)
+	[ClassInterface(ClassInterfaceType.None)] // Explicitly implements the interface
+	public class SimpleCom : ISimpleCom
+	{
+		public SimpleCom()
+		{
+			Console.WriteLine("SimpleCom instantiated.");
+		}
+
+		public string Test()
+		{
+			Console.WriteLine("Test method called.");
+			return "Hello from SimpleCom!";
+		}
+	}
+}
+
+
